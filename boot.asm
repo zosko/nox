f2d52d4a689cf263da100302aa029bbf2dd41211
Bosko <bosko.petreski@gmail.com>
2024-06-20 17:16:51 +0200

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -21,85 +21,138 @@
 
 BITS 16								; Tell NASM that this is a 16bit program
 org 0x7c00							; Start at memory address 0x7c00 whitc is where BIOS loads the bootloader into memory
+row_cursor:		db 0
+clumn_cursor:	db 0
 
-boot:
-	; Set up segment registers
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
-	mov ss, ax
-	mov fs, ax
-	mov gs, ax
+genesis:
+	mov ax, 0x0003	; BIOS.SetVideoMode 80x25 16-color text
+	int 0x10
 
-	mov sp, 0x7c00					; The stack grows down, so we set stack pointer to the starting memory address of our program
+	;mov ax, 0xb800
+	;mov ds, ax
+	;mov es, ax
 
-	; Read the entire first track of the floppy disk (~9.2kb) so that we can run more then 512 bytes of our program
-	mov ah, 0x02					; BIOS read from disk function
-	mov al, 18						; Read 18 sectors of floppy disk
-	mov ch, 0 						; Track 0
-	mov cl, 2						; The sector to start reading from
-	mov dh, 0						; Head (side) number
-	or dl, dl 						; dl is the drive to read from, and the BIOS provides it on startup
-	mov bx, 0x7e00					; 0x7e00 is a guarunteed open spot in memory, so this is where the read program data will go.
-	int 0x13						; Call the BIOS to read from disk based on what we have just defined
+	;mov     ax, 0xcf00
+    ;mov     cx, 10 * 25
+    ;rep     stosw
 
-	; We done all need to do in this part, so we can print message confirming it worked and fill the rest
-	; of the first secotor with zeros and 0xAA55 at the end.
-	mov si, bootmsg
-	mov ah, 0x0e					; BIOS video service print character function
-	mov bh, 0						; I cant say i know what this does, but im scared to remove it
+	mov  cx, 0x10      ; ReplicationCount
+	mov  bx, 0x002C    ; BH is DisplayPage (0) , BL is Attribute (BrightWhiteOnGreen)
+	;; 2C means =  2 means Green for background  C means Red for text
+	;; https://en.wikipedia.org/wiki/BIOS_color_attributes
+	mov  ax, 0x0941    ; BIOS.WriteCharacterAndAttribute, AL is ASCII ("A")
+	int  0x10
 
-.loop:
-	lodsb 							;Load a byte of the message into AL.
-                         					;Remember that DS is 0 and SI holds the
-                         					;offset of one of the bytes of the message.
-	cmp al, 0 						; Have we made it to the null terminator?
-	je .done						; If so, we are done
-	int 0x10						; BIOS video service
-	jmp .loop
+	;; predefined color
+	mov bx, 0x0022	; green for background and foreground
 
-.done:
-	; jmp $							; This is an infinite loop
-	jmp start						; This is an infinite loop here, but in reality you jump to the place you are going to 
-									; that outside the boot sector.
+start:
+	call read_key
+	cmp al, 's'	; DOWN key
+	je move_down
 
-bootmsg: 
-	db "floppy booted", 0			; Dont forget null terminators!
+	cmp al, 'w'	; UP key
+	je move_up
+
+	cmp al, 'a'	; LEFT key
+	je move_left
+
+	cmp al, 'd'	; RIGHT key
+	je move_right
+
+	cmp al, 0x31	; 1 key
+	je choose_color_red
+
+	cmp al, 0x32	; 2 key
+	je choose_color_green
+
+	cmp al, 0x33	; 3 key
+	je choose_color_blue
+
+	cmp al, 0x20	; SPACE key
+	je draw
+
+	jmp start
+
+display_char:
+	mov ah, 0x0e
+	mov bx, 0x000f
+	int 0x10
+	ret
+
+read_key:
+	mov ah, 0x00
+	int 0x16
+	ret
+
+choose_color_red:
+	mov bx, 0x00CC	; red for background and foreground
+	jmp start
+	ret
+
+choose_color_green:
+	mov bx, 0x0022	; green for background and foreground
+	jmp start
+	ret
+
+choose_color_blue:
+	mov bx, 0x0099	; blue for background and foreground
+	jmp start
+	ret
+
+draw:
+	mov cx, 0x01	; we want 1 replication of char
+	mov al, 0x20	; Will add empty SPACE 
+	mov ah, 0x09	; we apply WriteCharacterAndAttribute
+	int 0x10
+
+	jmp start
+	ret
+
+move_left:
+	mov al, [clumn_cursor]
+	dec al
+	mov [clumn_cursor], al
+
+	call move_cursor
+	ret
+
+move_right:
+	mov al, [clumn_cursor]
+	inc al
+	mov [clumn_cursor], al
+
+	call move_cursor
+	ret
+
+move_down:
+	mov al, [row_cursor]
+	inc al
+	mov [row_cursor], al
+
+	call move_cursor
+	ret
+
+move_up:
+	mov al, [row_cursor]
+	dec al
+	mov [row_cursor], al
+
+	call move_cursor
+	ret
+
+move_cursor:
+	mov dh, [row_cursor]	; Row
+	mov dl, [clumn_cursor]	; Column
+	mov bh, 0x00			; DisplayPage
+	mov ah, 0x02			; BIOS.SetCursorPosition
+	int 0x10
+
+	call start
+	ret
+
+done:
+	jmp $							; This is an infinite loop
 
 times 510 - ($-$$) db 0				; Fill the sector except for the actual code with zeros
 dw 0xAA55							; Boot sector signature
-
-; This is outside of the boot sector. Alltough much of the boot sector is zeros, it is always same size and bugs can happen if you go over
-; the designated size. Just put most of your stuff out here. It will most likly load.
-
-loaded:
-	db "This message is loaded after boot!", 0, 0 	; Dont forget null terminators! for some reason we need two
-
-start:
-	call Set_Video_Mode
-	mov si, loaded
-	call Print
-
-Print:
-	mov ah, 0x0e
-	mov bh, 0
-
-.loop:
-	lodsb
-	cmp al, 0
-	je .done
-	int 0x10
-	jmp .loop
-
-.done:
-	ret
-
-;
-; Set_Video_Mode: Set the display to VGA text mode
-; screen size 80x25 size 
-
-Set_Video_Mode:
-	mov ah, 00h
-	mov al, 03h
-	int 0x10
-	ret
